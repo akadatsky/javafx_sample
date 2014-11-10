ea7690d18d593a1fd58d4b433b3f1d35a76cc200
andreykadatsky <andrey.kadatsky@mobindusry.net>
2014-11-10 12:26:24 +0200

    add Scene

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,21 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        BorderPane group = new BorderPane();
+
+        Scene scene = new Scene(group, 1200, 700);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello World");
         primaryStage.show();
     }
 
