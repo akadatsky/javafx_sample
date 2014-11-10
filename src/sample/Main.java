3c85dba0f680033307ace4ef821084abff3fe13a
andreykadatsky <andrey.kadatsky@mobindusry.net>
2014-11-10 12:29:05 +0200

    add canvas

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,14 +1,9 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,23 +11,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        BorderPane mainGroup = new BorderPane();
+        Canvas canvas = new Canvas(1200, 700);
 
-        mainGroup.setTop(new Button("Button top"));
-        mainGroup.setRight(new Button("Button right"));
-        mainGroup.setBottom(new Button("Button bottom"));
-        mainGroup.setLeft(new Button("Button left"));
+        BorderPane group = new BorderPane();
+        group.setCenter(canvas);
 
-        BorderPane subGroup = new BorderPane();
-        subGroup.setTop(new Button("Button top subGroup"));
-        subGroup.setRight(new Button("Button right subGroup"));
-        subGroup.setBottom(new Button("Button bottom subGroup"));
-        subGroup.setLeft(new Button("Button left subGroup"));
-        subGroup.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
-
-        mainGroup.setCenter(subGroup);
-
-        Scene scene = new Scene(mainGroup, 1200, 700);
+        Scene scene = new Scene(group);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Hello World");
